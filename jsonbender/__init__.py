4a89acb81b7b38e8bc857caf17b3728954a0c25d
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2020-10-07 15:10:35 +0200

    add additional selectors to init

--- jsonbender/__init__.py
+++ jsonbender/__init__.py
@@ -1,7 +1,8 @@
 from jsonbender.core import Bender, Context, bend, BendingException
 from jsonbender.list_ops import FlatForall, Forall, Filter, Reduce
+from jsonbender.dict_ops import ForallDict
 from jsonbender.string_ops import Format
-from jsonbender.selectors import F, K, S, OptionalS, P, OptionalP, Element
+from jsonbender.selectors import F, K, S, OptionalS, P, OptionalP, Element, First, Second
 from jsonbender.control_flow import Alternation, If, Switch
 
 
