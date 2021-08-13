c44d166ad9df7e14fbeeb046285d2748a6962b6d
Roman Zimmermann <roman@more-onion.com>
2021-08-13 11:43:23 +0200

    Update README and package metadata

--- setup.py
+++ setup.py
@@ -3,19 +3,20 @@ from setuptools import find_packages, setup
 from jsonbender import __version__
 
 setup(
-    name="JSONBender",
+    name="jsonbender2",
     version=__version__,
     description="Library for transforming JSON data between different formats.",
-    author="Elias Tandel",
-    author_email="backend@onyo.com",
-    url="https://github.com/Onyo/jsonbender",
-    download_url="https://codeload.github.com/Onyo/jsonbender/tar.gz/" + __version__,
+    author="Roman Zimmermann",
+    author_email="torotil@gmail.com",
+    url="https://github.com/moreonion/jsonbender",
+    download_url="https://codeload.github.com/moreonion/jsonbender/tar.gz/" + __version__,
     keywords=["dsl", "edsl", "json"],
     packages=["jsonbender"],
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
